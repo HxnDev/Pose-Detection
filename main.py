7e7d5d9db7a899dc59586bfd8f9a49672047f494
Hassan Shahzad <chhxnshah@gmail.com>
2022-01-17 10:38:30 +0500

    Added Code Files

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(0)
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame")
+            continue
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        cv2.imshow('Pose Detection', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+cap.release()
